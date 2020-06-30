9ba458dafb864560b0ed86b3c63f4f6d9d063e03
pamelanancupil <pamela.nancupilv@gmail.com>
2020-06-30 16:41:23 -0400

    Configuración Angular y Firebase

---
+++ burgerQueen/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { environment } from '../environments/environment';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFirestoreModule,
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ burgerQueen/src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCLPy3Oq4OgWK3Ti38rH3YBX6RoF2MBDws",
+    authDomain: "burgerqueen-de02a.firebaseapp.com",
+    databaseURL: "https://burgerqueen-de02a.firebaseio.com",
+    projectId: "burgerqueen-de02a",
+    storageBucket: "burgerqueen-de02a.appspot.com",
+    messagingSenderId: "593176371798",
+    appId: "1:593176371798:web:be9345dbe9fb1f5ec011f7",
+    measurementId: "G-ZRHGNJED8J"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
