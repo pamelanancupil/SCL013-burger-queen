0b8c15793f21f3fee7a443eb7db9257b16fa6ea1
pamelanancupil <pamela.nancupilv@gmail.com>
2020-07-01 18:59:26 -0400

    Rutas

--- burgerQueen/src/app/app.module.ts
+++ burgerQueen/src/app/app.module.ts
@@ -11,6 +11,28 @@ import { HomeComponent } from './components/home/home.component';
 import { WaiterComponent } from './components/waiter/waiter.component';
 import { ChefComponent } from './components/chef/chef.component';
 
+import { RouterModule, Routes } from '@angular/router';
+
+const route: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'waiter',
+    component: WaiterComponent
+  },
+  {
+    path: 'chef',
+    component: ChefComponent
+  }
+]
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -23,6 +45,7 @@ import { ChefComponent } from './components/chef/chef.component';
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
     AngularFirestoreModule,
+    RouterModule.forRoot(route)
     
   ],
   providers: [],
