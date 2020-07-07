43a4ee63253775380f47493dce51a306b091f23f
pamelanancupil <pamela.nancupilv@gmail.com>
2020-07-07 11:05:15 -0400

    Data.json

--- burgerQueen/src/app/components/food-menu/food-menu.component.ts
+++ burgerQueen/src/app/components/food-menu/food-menu.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as menu from '../../../assets/JSON/data.json';
+//const menu = require ('../../../assets/JSON/data.json');
+
+
 @Component({
   selector: 'app-food-menu',
   templateUrl: './food-menu.component.html',
@@ -7,9 +11,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FoodMenuComponent implements OnInit {
 
+  data: any = menu;
+  
+  
   constructor() { }
-
+  mostrarMenu(){
+    console.log(menu)
+    console.log(this.data)
+  }
   ngOnInit(): void {
+    console.log(menu)
+    console.log(this.data)
   }
 
 }
