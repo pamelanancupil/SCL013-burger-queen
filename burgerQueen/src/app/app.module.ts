2a730917b05e747708059988ab7fcc7c9d4bddaa
marce-8888 <mtancarao@outlook.cl>
2020-07-15 11:55:27 -0400

    Firebase

--- burgerQueen/src/app/app.module.ts
+++ burgerQueen/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireDatabaseModule } from '@angular/fire/database';
@@ -51,6 +52,7 @@ const route: Routes = [
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
     AngularFirestoreModule,
