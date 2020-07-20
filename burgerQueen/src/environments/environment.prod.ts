caa95f51c0d4c45b2cb77411e5061ebcfd9e294d
marce-8888 <mtancarao@outlook.cl>
2020-07-19 21:10:35 -0400

    deploy

--- burgerQueen/src/environments/environment.prod.ts
+++ burgerQueen/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCLPy3Oq4OgWK3Ti38rH3YBX6RoF2MBDws",
+    authDomain: "burgerqueen-de02a.firebaseapp.com",
+    databaseURL: "https://burgerqueen-de02a.firebaseio.com",
+    projectId: "burgerqueen-de02a",
+    storageBucket: "burgerqueen-de02a.appspot.com",
+    messagingSenderId: "593176371798",
+    appId: "1:593176371798:web:be9345dbe9fb1f5ec011f7",
+    measurementId: "G-ZRHGNJED8J"
+  }
 };
